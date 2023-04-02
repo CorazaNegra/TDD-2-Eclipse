202128e0a91e1ce501d39e8676159e2cd2978ad5
Francisco Salar Pérez <frasalper@alu.edu.gva.es>
2023-04-02 16:59:32 +0200

    Creación test acelerar y método acelerar

--- TddPracticaCoche/src/Tests/TestCoche.java
+++ TddPracticaCoche/src/Tests/TestCoche.java
@@ -9,8 +9,16 @@ class TestCoche {
 
 	@Test
 	void test_al_crear_un_coche_su_velocidad_es_cero() {
-		Coche nuevoCoche =new Coche();
+		Coche nuevoCoche = new Coche();
 		Assertions.assertEquals(0, nuevoCoche.velocidad);
 	}
+
+	@Test
+	void test_al_acelerar_un_coche_su_velocidad_aumenta() {
+		Coche nuevoCoche = new Coche();
+		nuevoCoche.acelerar(30);
+		Assertions.assertEquals(30, nuevoCoche.velocidad);
+	}
+
 
 }
